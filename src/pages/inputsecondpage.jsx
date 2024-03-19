7e0c2aab3171155f73fbb64aa2b95e76d6ba4ba6
Yash <yashkashyap1555@gmail.com>
2024-03-19 14:15:43 +0530

    somechange

--- src/pages/inputsecondpage.jsx
+++ src/pages/inputsecondpage.jsx
@@ -6,13 +6,13 @@ const InputSecondPage = () => {
     password: "",
     email: "",
   };
-
+  // first state
   const [details, setDetails] = useState(InitialState);
-  
-  const [allUserdetails, setAlluserDetails] = useState([])
-  console.log(allUserdetails)
-  
   console.log(details);
+
+  // second state to insert the user value array in UI list
+  const [allUserdetails, setAlluserDetails] = useState([]);
+  console.log(allUserdetails);
 
   const ChangeValue = (e) => {
     //first of follow name and value to destructure on event its important
@@ -24,76 +24,73 @@ const InputSecondPage = () => {
     });
     // jab object ke ander value pas karni hai to (...spreadoperator) and [name] : value to pas the all value in object then it is on itF
   };
-  
-  const ChangeSubmit = () =>{
 
-  }
+  const ChangeSubmit =(e)=> {
+    e.preventDefault()
+    setAlluserDetails([
+      ...allUserdetails, details
+    ]);
+  };
 
-   const {name, password, email} = details
-   //name, password, email ko destructure isly kiya kyuki niche usko use kiya hai
+  const { name, password, email } = details;
+  //name, password, email ko destructure isly kiya kyuki niche usko use kiya hai
   return (
     <>
-     <form>
-      <h2>Registration form and data insert in list</h2>
-      <div>
-        <input
-          name="name"
-          value={name}
-          type="text"
-          placeholder="Enter your name"
-          onChange={ChangeValue}
-        />
-      </div>
+      <form onSubmit={ChangeSubmit}>
+        <h2>Registration form and data insert in list</h2>
+        <div>
+          <input
+            name="name"
+            value={name}
+            type="text"
+            placeholder="Enter your name"
+            onChange={ChangeValue}
+          />
+        </div>
+
+        <div>
+          <input
+            name="email"
+            value={email}
+            type="email"
+            placeholder="Enter your email"
+            onChange={ChangeValue}
+          />
+        </div>
+
+        <div>
+          <input
+            name="password"
+            value={password}
+            type="password"
+            placeholder="Enter your password"
+            onChange={ChangeValue}
+          />
+        </div>
+        <button type="submit">submit</button>
+      </form>
 
       <div>
-        <input
-          name="email"
-          value={email}
-          type="email"
-          placeholder="Enter your email"
-          onChange={ChangeValue}
-        />
-      </div>
-
-      <div>
-        <input
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Enter your password"
-          onChange={ChangeValue}
-        />
-      </div>
-      <button type="submit">submit</button>
-    </form>
-
-    <div>
-    <table>
-        <tr>
+        <table>
+          <tr>
             <th>Name</th>
-            <th>Age</th>
-            <th>Gender</th>
-        </tr>
-        <tr>
-            <td>{name}</td>
-            <td>{email}</td>
-            <td>{password}</td>
-        </tr>
-        <tr>
-            <td>Megha</td>
-            <td>19</td>
-            <td>Female</td>
-        </tr>
-        <tr>
-            <td>Subham</td>
-            <td>25</td>
-            <td>Male</td>
-        </tr>
-    </table>
-    </div>
+            <th>Email</th>
+            <th>Password</th>
+          </tr>
+            {allUserdetails?.map((value, i) =>(
+                <tr key={i}>
+                <td>{value.name}</td>
+                <td>{value.email}</td>
+                <td>{value.password}</td>
+              </tr>
+            )
+
+            )}
+          
+
+        </table>
+      </div>
     </>
-   
-    
   );
 };
 export default InputSecondPage;
